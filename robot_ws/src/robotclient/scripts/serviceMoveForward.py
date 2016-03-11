3c808e5e8ef7de59ffd169a46eb3693eee6cd420
Oscar <oscar.beronius@gmail.com>
2016-03-11 17:42:24 +0100

    Fixed bugs in moveRobot service

--- robot_ws/src/robotclient/scripts/serviceMoveForward.py
+++ robot_ws/src/robotclient/scripts/serviceMoveForward.py
@@ -2,12 +2,16 @@
 PKG = 'numpy'
 import roslib; roslib.load_manifest(PKG)
 
+from robotclient.srv import *
+
 import rospy
 from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import numpy as np
+
+
 
 
 
