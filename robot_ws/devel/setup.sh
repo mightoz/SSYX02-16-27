a9aa6a51f034485615a138ffc4247a35a9b6d508
Oscar <oscar.beronius@gmail.com>
2016-03-15 09:17:44 +0100

    Merge.

--- robot_ws/devel/setup.sh
+++ robot_ws/devel/setup.sh
@@ -8,7 +8,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/multipos/SSYX02-16-27/robot_ws/devel}
+: ${_CATKIN_SETUP_DIR:=/home/multipos2/SSYX02-16-27/robot_ws/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
