7ff81c2bacef284c1b84ab51eba155bb31984ea3
Mightoz <oscar.beronius@gmail.com>
2016-04-19 15:58:15 +0200

    Changed function and variable names to follow standard Python conventions.

--- robot_ws/src/masterclient/src/MainController.py
+++ robot_ws/src/masterclient/src/MainController.py
@@ -17,36 +17,37 @@ from robotclient.srv import *
 
 class MainController():
     def __init__(self):
-        # alignrobot1.0
-
-        # Choose number of self.nodes
-        numberofNodes = 4
         self.nodes = []  # Array containing pointers to instances of all the Nodes
-        for i in range(0, numberofNodes):
+
+
+    def align_robots_1(self):
+        # Choose number of self.nodes
+        number_of_nodes = 4
+        for i in range(0, number_of_nodes):
             self.nodes.append(Node.Node(i))  # create instances of all the Nodes
         else:
             pass
 
         # Set while condition too True.
-        notFinished = True
-        while (notFinished):
+        not_finished = True
+        while (not_finished):
             # Loop through all of the robots
-            for i in range(1, numberofNodes - 1):
+            for i in range(1, number_of_nodes - 1):
                 # nextPosition = np.array([], dtype=np.float32)
                 # Calulate correctposition for robot
 
-                possiblenextPosition = self.CorrectPos2(self.nodes[i])
-                print possiblenextPosition
-                # Check if position is within a radius of 0.1m of possiblenextPosition
-                if (np.linalg.norm(self.nodes[i].GetCoords() - possiblenextPosition) > 0.1):
+                possible_next_position = self.correct_pos(self.nodes[i])
+                print possible_next_position
+                # Check if position is within a radius of 0.1m of possible_next_position
+                if (np.linalg.norm(self.nodes[i].GetCoords() - possible_next_position) > 0.1):
                     print "Tries to move"
-                    self.MoveAToB2(self.nodes[i], possiblenextPosition)  # Otherwise move
+                    self.move_a_to_b(self.nodes[i], possible_next_position)  # Otherwise move
                     # TODO: ONLY CHECK AND THEN MOVE.
             else:
                 pass
                 # For printing
         colors = ['gx', 'ro', 'bo', 'gx']
-        for i in (0, numberofNodes):
+        for i in (0, number_of_nodes):
             print "Recorded positions for Node %s are %s" % (self.nodes[i], self.nodes[i].GetRecordedPositions())
             print "Recorded X positions for Node %s are %s" % (self.nodes[i], self.nodes[i].getRecordedXPositions())
             print "Recorded Y positions for Node %s are %s" % (self.nodes[i], self.nodes[i].getRecordedYPositions())
@@ -59,46 +60,50 @@ class MainController():
         plt.axis([-2, 3.5, -3, 3.5])
         plt.show()
 
+    def align_robots_2(self):
+
+
+
     # Find correctposition for robot
-    def correct_pos_2(self, robot):
-        correctPosition = np.array([], dtype=np.float32)
+    def correct_pos(self, robot):
+        correct_position = np.array([], dtype=np.float32)
         left = robot.GetLeftNeighbor()
         right = robot.GetRightNeighbor()
-        correctPosition = (self.nodes[left].GetCoords() + self.nodes[
+        correct_position = (self.nodes[left].GetCoords() + self.nodes[
             right].GetCoords()) / 2  # Calculates correct position
-        return correctPosition
+        return correct_position
 
     # Move a robot from point A to B
-    def MoveAToB2(self, robot, target):
+    def move_a_to_b(self, robot, target):
         print "Moving , activerobot: ", robot
-        initialPos = robot.GetCoords()
+        initial_pos = robot.GetCoords()
         robot.DriveForward(0.1)
-        self.RunNextSegment2(robot, initialPos, target)
+        self.run_next_segment(robot, initial_pos, target)
 
         return robot.GetRecordedPositions
 
-    def RunNextSegment2(self, robot, previousPos, targetPos):
-        target = targetPos
+    def run_next_segment(self, robot, previous_pos, target_pos):
+        target = target_pos
         current = robot.GetCoords()
 
         if (not (((np.absolute(target[0] - current[0])) <= 0.15) & (
                     (np.absolute(target[1] - current[1])) <= 0.15))):
             length = math.sqrt(math.pow((target[0] - current[0]), 2) + math.pow((target[1] - current[1]), 2))
-            deg = self.calculateAngle2(previousPos, current, target)
+            deg = self.calculate_angle(previous_pos, current, target)
             if length <= 0.1:
                 robot.Rotate(deg)
                 robot.DriveForward(length)
             else:
                 robot.Rotate(deg)
                 robot.DriveForward(0.1)
-            self.RunNextSegment2(robot, current, target)
+            self.run_next_segment(robot, current, target)
         else:
             return robot
 
-    def calculateAngle2(self, previousPos, currentPos, targetPos):
-        previous = previousPos
-        current = currentPos
-        target = targetPos
+    def calculate_angle(self, previous_pos, current_pos, target_pos):
+        previous = previous_pos
+        current = current_pos
+        target = target_pos
         print "Previous position: ", previous
         print "Current position: ", current
         print "Target: ", target
@@ -122,20 +127,20 @@ class MainController():
         theta = np.arccos(np.sum((target-fst)*(snd-fst))/(np.sqrt(np.sum((target-fst)**2))*np.sqrt(np.sum((snd-fst)**2))))
         return direction*theta
         """
-        dotProd = (current[0] - previous[0]) * (target[0] - previous[0]) + (current[1] - previous[1]) * (
+        dot_prod = (current[0] - previous[0]) * (target[0] - previous[0]) + (current[1] - previous[1]) * (
         target[1] - previous[1])
         lengthA = math.sqrt(math.pow((current[0] - previous[0]), 2) + math.pow((current[1] - previous[1]), 2))
         lengthB = math.sqrt(math.pow((target[0] - previous[0]), 2) + math.pow((target[1] - previous[1]), 2))
 
         # Calculate degrees to turn
-        theta = math.acos(dotProd / (lengthA * lengthB))
+        theta = math.acos(dot_prod / (lengthA * lengthB))
 
         print rospy.get_name(), "Turningdegree: ", theta
         print rospy.get_name(), "Direction: ", direction
 
-        turningDegree = direction * theta  # (np.pi - math.asin(lengthB*(math.sin(theta)/lengthToTarget)))
+        turning_degree = direction * theta  # (np.pi - math.asin(lengthB*(math.sin(theta)/lengthToTarget)))
 
-        return turningDegree
+        return turning_degree
 
 
 if __name__ == '__main__':
