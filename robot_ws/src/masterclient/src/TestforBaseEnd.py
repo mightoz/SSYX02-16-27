1d7fcf8b9fcf2ef31a09b49f8d06d1148fe8af74
Oscar <oscar.beronius@gmail.com>
2016-04-19 21:28:19 +0200

    Debugged and tested services for changing Base/End, works/Agnes

--- robot_ws/src/masterclient/src/TestforBaseEnd.py
+++ robot_ws/src/masterclient/src/TestforBaseEnd.py
@@ -17,18 +17,18 @@ from masterclient.srv import *
 
 
 def test():
-	tmpPos = np.empty([], dtype=np.float32)	
-	rospy.wait_for_service('get_coord0')
+    tmpPos = np.empty([], dtype=np.float32)	
+    rospy.wait_for_service('get_coord0')
     basepos = rospy.ServiceProxy('get_coord0', GetCoord)
     f = Floats()
-    f = get_coords(1)
+    f = basepos()
     tmpPos = f.data.data
     print "Base station is", tmpPos
-    f = Floats()
-    f = get_coords(1)
-    tmpPos = f.data.data
     rospy.wait_for_service('get_coord3')
     endpos = rospy.ServiceProxy('get_coord3', GetCoord)
+    f = Floats()
+    f = endpos()
+    tmpPos = f.data.data 
     print "End station is", tmpPos
 
 
