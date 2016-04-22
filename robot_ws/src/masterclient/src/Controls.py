452c49b25b40a700be8635a5af6cff468e822625
Marcus <marmalm@student.chalmers.se>
2016-04-22 16:17:49 +0200

    added documentation for all methods and changed names to fit naming conventions

--- robot_ws/src/masterclient/src/Controls.py
+++ robot_ws/src/masterclient/src/Controls.py
@@ -35,6 +35,75 @@ def get_rot_dir(theta, curr_pos, tar_pos):
             return -1
 
 
+def get_trans_magn_2(current, target, t):
+    """
+    
+    :param current:
+    :param target:
+    :param t:
+    :return:
+    """
+    length_to_target = np.linalg.norm(target-current)
+
+    # X function of Z and length
+
+    if length_to_target > 0.5:
+        x = 1
+    elif length_to_target > 0.25:
+        x = 0.5 * length_to_target / t
+    elif length_to_target > 0.05:
+        x = 0.25 * length_to_target / t
+    else:
+        x = 0
+
+    return x
+
+
+def get_rot_magn_2(theta, current, target, t):
+    """
+
+    :param theta:
+    :param current:
+    :param target:
+    :param t:
+    :return:
+    """
+    direction = get_rot_dir(theta, current, target)
+
+    a = np.array([[np.cos(theta), np.sin(theta)],
+                  [np.sin(theta), -np.cos(theta)]])
+    b = target-current
+    (alpha, beta) = np.linalg.solve(a, b)
+    (alpha, beta) = (np.abs(alpha), np.abs(beta))
+    length_to_target = np.linalg.norm(target-current)
+
+    phi = np.arctan(beta / alpha)
+
+    x = np.arctan((target[1]-current[1])/(target[0]-current[0]))
+
+    if np.cos(theta-x) < 0:
+        print ('phi', phi)
+        phi = np.pi-phi
+
+    if phi > (45*np.pi/180):
+        za = 1
+    elif phi > (20*np.pi/180):
+        za = 0.5
+    elif phi > (5*np.pi/180):
+        za = 0.25
+    else:
+        print "z was zero"
+        za = 0
+        print ('length_to_target:', str(length_to_target))
+    print za
+
+    if length_to_target < 0.05:
+        z = 0
+    else:
+        z = phi / t * za * direction
+    return z
+
+
 class Controls(object):
     def __init__(self):
         self.x_min = None
@@ -47,18 +116,7 @@ class Controls(object):
         self.ok_dist = None
 
     def initiate(self, x_min, x_max, z_min, z_max, k, t_x, t_z, ok_dist):
-        """
 
-        :param x_min: minimum velocity [m/s]
-        :param x_max: maximum velocity [m/s]
-        :param z_min: minimum angular velocity [rad/s]
-        :param z_max: maximum angular velocity [rad/s]
-        :param k: gradient descent coefficient (0<k<1)
-        :param t_x: time to reach target pos if as if the robot was facing its target pos
-        :param t_z: time to rotate to face target pos
-        :param ok_dist: minimum distance to target pos that will make the robot move
-        :return:
-        """
         self.x_min = x_min
         self.x_max = x_max
         self.z_min = z_min
@@ -100,23 +158,6 @@ class Controls(object):
             else:
                 return dist / self.t_x * (np.pi - phi) / np.pi
 
-    def get_trans_magn_2(self, current, target, t, phi):
-
-        lengthtotarget = np.linalg.norm(target-current)
-
-        # X FUNKTION AV Z OCH LENGTH
-
-        if lengthtotarget > 0.5:
-            x = 1
-        elif lengthtotarget > 0.25:
-            x = 0.5 * lengthtotarget / t
-        elif lengthtotarget > 0.05:
-            x = 0.25 * lengthtotarget / t
-        else:
-            x = 0
-
-        return x
-
     def get_rot_magn_1(self, theta, curr_pos, tar_pos):
         """
 
@@ -126,9 +167,9 @@ class Controls(object):
         :return: The rotation for the next iteration
         """
         y = tar_pos - curr_pos
-        A = np.array([[np.cos(theta), np.sin(theta)],
+        a = np.array([[np.cos(theta), np.sin(theta)],
                       [np.sin(theta), -np.cos(theta)]])
-        x = np.linalg.solve(A, y)
+        x = np.linalg.solve(a, y)
 
         d = np.array([x[0] * np.cos(theta), x[0] * np.sin(theta)])
         if np.linalg.norm(y) > 1e-40:
@@ -146,105 +187,143 @@ class Controls(object):
         else:
             return phi / self.t_z
 
-    def get_rot_magn_2(self, theta, current, target, t):
-
-        direction = get_rot_dir(theta, current, target)
-
-        a = np.array([[np.cos(theta), np.sin(theta)],[np.sin(theta), -np.cos(theta)]])
-        b = target-current
-        (alfa,beta) = np.linalg.solve(a,b)
-        (alfa,beta) = (np.abs(alfa),np.abs(beta))
-        lengthtotarget = np.linalg.norm(target-current)
-
-        phi = np.arctan(beta / alfa)
-
-        x = np.arctan((target[1]-current[1])/(target[0]-current[0]))
-
-
-        if np.cos(theta-x) < 0:
-            print ('phi', phi)
-            phi = np.pi-phi
-
-        if phi > (45*(np.pi)/180):
-            za = 1
-        elif phi > (20*(np.pi)/180):
-            za = 0.5
-        elif phi > (5*(np.pi)/180):
-            za = 0.25
-        else:
-            print "z blev noll"
-            za = 0
-            print ('lengthtotarget:', str(lengthtotarget))
-        print za
-
-        if lengthtotarget < 0.05:
-            z = 0
-        else:
-            z = phi / t * za * direction
-        return z
-
-    def calc_controls(self, theta, curr_pos, neighbour_1_pos, neighbour_2_pos, wheel_dist):
+    def calc_controls(self, theta, curr_pos, neighbour_1_pos, neighbour_2_pos):
         """
 
         :param theta: orientation relative to the x-axis in the coordinate grid. positive ccw
         :param curr_pos: current position
         :param neighbour_1_pos: position of neighbour 1
         :param neighbour_2_pos: position of neighbour 2. if only 1 neighbour exist, set it to 0
-        :param wheel_dist: distance between the front wheels
         :return: the next controls; X, Z
         """
-
         tar_pos = self.find_next_pos(curr_pos, neighbour_1_pos, neighbour_2_pos)
         next_z = get_rot_dir(theta, curr_pos, tar_pos) * self.get_rot_magn_1(theta, curr_pos, tar_pos)
         next_x = self.get_trans_magn_1(curr_pos, tar_pos, np.abs(next_z * self.t_z))
-        print "Robot has", tar_pos, next_x , next_z
+        print "Robot has", tar_pos, next_x, next_z
         print "neighbour", neighbour_1_pos, neighbour_2_pos
-        return next_x, next_z ##-wheel_dist*next_z/2
+        return next_x, next_z
 
     def set_x_max(self, val):
+        """
+
+        :param val: maximum velocity [m/s]
+        :return:
+        """
         self.x_max = val
 
     def set_x_min(self, val):
+        """
+
+        :param val: minimum velocity [m/s]
+        :return:
+        """
         self.x_min = val
 
     def set_z_max(self, val):
+        """
+
+        :param val: maximum angular velocity [rad/s]
+        :return:
+        """
         self.z_max = val
 
     def set_z_min(self, val):
+        """
+
+        :param val: minimum angular velocity [rad/s]
+        :return:
+        """
         self.z_min = val
 
     def set_k(self, val):
-        self.k = val
+        """
+
+        :param val: gradient descent coefficient (0<k<1)
+        :return:
+        """
+        if val > 1:
+            self.k = 1
+        elif val < 0:
+            self.k = 0
+        else:
+            self.k = val
 
     def set_t_x(self, val):
+        """
+
+        :param val: time to reach target pos if as if the robot was facing its target pos
+        :return:
+        """
         self.t_x = val
 
     def set_t_z(self, val):
+        """
+
+        :param val: time to rotate to face target pos
+        :return:
+        """
         self.t_z = val
 
     def set_ok_dist(self, val):
+        """
+
+        :param val: minimum distance to target pos that will make the robot move
+        :return:
+        """
         self.ok_dist = val
 
     def get_x_max(self):
+        """
+
+        :return: maximum velocity
+        """
         return self.x_max
 
     def get_x_min(self):
+        """
+
+        :return: minimum velocity
+        """
         return self.x_min
 
     def get_z_max(self):
+        """
+
+        :return: maximum value of angular velocity
+        """
         return self.z_max
 
     def get_z_min(self):
+        """
+
+        :return: minimum value of angular velocity
+        """
         return self. z_min
 
     def get_k(self):
+        """
+
+        :return: gradient descent coefficient
+        """
         return self.k
 
     def get_t_x(self):
+        """
+
+        :return: time to reach target pos if as if the robot was facing its target pos
+        """
         return self.t_x
 
     def get_t_z(self):
+        """
+
+        :return: time to rotate to face target pos
+        """
         return self.t_z
 
     def get_ok_dist(self):
+        """
+
+        :return: minimum distance to target pos that will make the robot move
+        """
         return self.ok_dist
