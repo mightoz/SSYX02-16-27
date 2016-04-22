c8e9a8c47241d7524a677e1f8e100fd15f66cc9b
Marcus <marmalm@student.chalmers.se>
2016-04-22 17:47:56 +0200

    turned the scripts into classes and started tidying up

--- robot_ws/src/robotclient/src/ConnectionRequest.py
+++ robot_ws/src/robotclient/src/ConnectionRequest.py
@@ -2,44 +2,63 @@ PKG = 'numpy'
 import numpy as np
 import rcmGetConfig
 import rcmSetConfig
+import socket
 
 
-def connectReq(reqId, doPrint):
-    """
+class ConnectionRequest(object):
 
-    :param reqId: The ID of the RCM:s (wired) ip. If the RCM:s ip 192.168.1.XXX, then XXX is the id
-    :param doPrint: The config will be printed if doPrint is 1. It will not be printed if it is 0.
-    :return: The socket that has been created and the ip of the RCM that has been constructed from its id.
-    """
-    reqIp = '192.168.1.'+str(reqId)
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.req_ip = None
+        self.config = None
 
-    config = np.empty(0)
-    while np.size(config) == 0:
-        [s, config] = rcmGetConfig.getConf(reqIp)
+    def set_req_ip(self, val):
+        if val[0:10] == '192.168.1.':
+            self.req_ip = val
+        else:
+            print 'Invalid IP'
 
-    """ Uncomment this to change configuration parameters.
-        Leave commented to accept the defaults from the radio """
-    # config[2] = 7  # config.pii  # This must match the responder
-    # config[4] = 6  # config.codeChnl  # This must match the responder
-    # config[8] = 10;  # config.txPwr
-    config[7] = np.array([1], dtype=np.uint16)  # configFlags, make sure scan data is set (not default)
-    config[12] = np.array([0], dtype=np.uint8)  # configPersistFlag, Don't change this in flash
+    def get_socket(self):
+        return self.s
 
-    status = rcmSetConfig.setConf(s, reqIp, config)
-    if doPrint:
-        print config
-    return [s, reqIp]
+    def get_req_ip(self):
+        return self.req_ip
 
+    def connect_req(self, do_print):
+        """
 
-def dcReq(s, doPrint):
-    """
+        :param req_id: The ID of the RCM:s (wired) ip. If the RCM:s ip 192.168.1.XXX, then XXX is the id
+        :param do_print: The config will be printed if do_print is 1. It will not be printed if it is 0.
+        :return: The socket that has been created and the ip of the RCM that has been constructed from its id.
+        """
 
-    :param s: The socket that is t obe closed
-    :param doPrint: A message saying that the socket is closing if this parameter is 1. The message will not be
-    printed if it is 0.
-    :return:
-    """
-    if doPrint:
-        print 'closing socket'
-    s.close
-    return
+        while self.config is None:
+            self.s, self.config = rcmGetConfig.getConf(self.req_ip)
+
+        """
+        Uncomment this to change configuration parameters.
+        Leave commented to accept the defaults from the radio
+        """
+        # config[2] = 7  # config.pii  # This must match the responder
+        # config[4] = 6  # config.codeChnl  # This must match the responder
+        # config[8] = 10;  # config.txPwr
+        self.config[7] = np.array([1], dtype=np.uint16)  # configFlags, make sure scan data is set (not default)
+        self.config[12] = np.array([0], dtype=np.uint8)  # configPersistFlag, Don't change this in flash
+
+        rcmSetConfig.setConf(self.s, self.req_ip, self.config)
+        if do_print:
+            print self.config
+        return self.s, self.req_ip
+
+    def dc_req(self, do_print):
+        """
+
+        :param do_print: A message saying that the socket is closing if this parameter is 1. The message will not be
+        printed if it is 0.
+        :return:
+        """
+        if do_print:
+            print 'closing socket'
+        if self.s is not None:
+            self.s.close()
+        return
--- robot_ws/src/robotclient/src/Measure.py
+++ robot_ws/src/robotclient/src/Measure.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 PKG = 'robotclient'
 
-import roslib; roslib.load_manifest(PKG)
+import roslib;
+
+roslib.load_manifest(PKG)
 import rospy
 from rospy.numpy_msg import numpy_msg
 from robotclient.msg import *
@@ -10,31 +12,37 @@ import ConnectionRequest
 import RunLocateRobot
 import numpy as np
 import matplotlib.pyplot as plt
-#Initializes node and creates publisher. 
+
+
+class Measure(object):
+
+    def __init__(self):
+        self.connect_request = ConnectionRequest.ConnectionRequest()
+# Initializes node and creates publisher.
 def talker():
-    pub = rospy.Publisher('coordinates', numpy_msg(Floats),queue_size=10)
+    pub = rospy.Publisher('coordinates', numpy_msg(Floats), queue_size=10)
     rospy.init_node('talker', anonymous=True)
     r = rospy.Rate(10)
 
     while not rospy.is_shutdown():
         a = main()
         pub.publish(a)
-	r.sleep()
-#Runs scripts to retrieve coordinates for robot.
+        r.sleep()
+
+
+# Runs scripts to retrieve coordinates for robot.
 def main():
     s, reqIp = ConnectionRequest.connectReq(101, 0)  # connect to the RCM that is connected via ethernet cable
 
     # Params: UWB-transceiver ip and coordinates for each transceiver.
     pos = RunLocateRobot.RunLocRob(s, reqIp, np.array([[106], [114], [109]]),
-                               np.array([[-1, 2, 1], [1, 0, -2]]), 4, 1e-6, 1e-6, 0)
-    posNp = np.array(pos, dtype = np.float32)
-    posx = posNp[0]
-    posy = posNp[1]
+                                   np.array([[-1, 2, 1], [1, 0, -2]]), 4, 1e-6, 1e-6, 0)
+    posNp = np.array(pos, dtype=np.float32)
 
-    f=Floats()
+    f = Floats()
     f.data = posNp
 
-    ConnectionRequest.dcReq(s, 0)  # close the socket that was opened above.
+    ConnectionRequest.dc_req(s, 0)  # close the socket that was opened above.
 
     return f
 
