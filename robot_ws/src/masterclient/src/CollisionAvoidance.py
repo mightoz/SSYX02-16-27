5a9933afb62d005b37ce027b27884765e832c235
Marcus <marmalm@student.chalmers.se>
2016-04-30 01:23:36 +0200

    started working on code to evade collision

--- robot_ws/src/masterclient/src/CollisionAvoidance.py
+++ robot_ws/src/masterclient/src/CollisionAvoidance.py
@@ -41,6 +41,44 @@ def predict(pos, theta, x, z, time_step):
     return x_k_k1, theta
 
 
+def get_rot_dir(theta, q, rob_2_pos):
+    """
+
+    :param theta: angle
+    :param q: point on the circle with radius abstol centered at rob_2_pos [q_x, q_y]
+    :param rob_2_pos: robot 2 position [x, y]
+    :return:
+    """
+    # calculate the angle between the x-axis and the line intersecting end_pos and start_pos
+    if np.abs(q[0] - rob_2_pos[0]) > 1e-30:
+        z1 = q - rob_2_pos
+        z2 = np.array([1, 0])
+        if q[1] >= rob_2_pos[1]:  # 0 <= phi <= pi
+            phi = np.arccos(np.dot(z1, z2) / (np.linalg.norm(z1) * np.linalg.norm(z2)))
+        else:  # pi < phi < 2*pi
+            phi = 2 * np.pi - np.arccos(np.dot(z1, z2) / (np.linalg.norm(z1) * np.linalg.norm(z2)))
+    else:
+        phi = np.sign(q[1] - rob_2_pos[1]) * np.pi / 2
+
+    # determine the rotation direction (+1 ccw, -1 cw)
+    if 3*np.pi/2 <= phi < 2*np.pi:
+        if np.mod(theta, 2*np.pi) < phi:
+            angle = phi - np.pi/2
+        else:
+            angle = phi + np.pi/2
+    elif 0 <= phi < np.pi/2:
+        if np.mod(theta, 2*np.pi) >= phi:
+            angle = phi + np.pi/2
+        else:
+            angle = phi - np.pi/2
+    else:  # np.pi/2 <= phi < 3*np.pi/2
+        if np.mod(theta, 2*np.pi) >= phi:
+            angle = phi + np.pi/2
+        else:
+            angle = phi - np.pi/2
+    return angle
+
+
 def __tar_fun_lin_lin__(pos_1, theta_1, x_1, pos_2, theta_2, x_2, t_2, t_1):  # rob1 lin, rob2 lin
     result = ((pos_1-pos_2)[0] + x_1 * np.cos(theta_1) * t_1 - x_2 * np.cos(theta_2) * t_2) ** 2 + \
              ((pos_1-pos_2)[1] + x_1 * np.sin(theta_1) * t_1 - x_2 * np.sin(theta_2) * t_2) ** 2
@@ -150,11 +188,6 @@ class CollisionAvoidance(object):
                         return False
                     residual = __tar_fun_rot_rot__(pos_1, theta_1, x_1, z_1, pos_2, theta_2, x_2, z_2, t_2, t_1)
                     if np.abs(residual) < self.abs_tol:
-                        print 'res ', residual
-                        asd, dsa = predict(pos_1, theta_1, x_1, z_1, t_1)
-                        asdd, dsad = predict(pos_2, theta_2, x_2, z_2, t_2)
-                        plt.plot(asd[0, 0], asd[2, 0], 'mo')
-                        plt.plot(asdd[0, 0], asdd[2, 0], 'co')
                         return True
             else:  # rob1 rot rob2 lin
                 while np.abs(last_residual-residual) > self.rel_tol:
@@ -192,52 +225,64 @@ class CollisionAvoidance(object):
                         return True
         return False
 
+    def calc_new_controls(self, pos_1, theta_1, x_1, z_1, pos_2, theta_2, x_2, z_2, time_exec):
+        new_x_1 = x_1
+        new_z_1 = z_1
+        new_x_2 = x_2
+        new_z_2 = z_2
+        collide1 = self.gradient_descent(pos_1, theta_1, new_x_1, new_z_1, pos_2, theta_2, new_x_2, new_z_2, time_exec)
+        if collide1:
+            print 'collide1'
+            new_x_2 = 0
+            new_z_2 = 0
+            collide2 = self.gradient_descent(pos_1, theta_1, new_x_1, new_z_1, pos_2, theta_2, new_x_2, new_z_2, time_exec)
+            if collide2:
+                print 'collide2'
+                t_2 = 0
+                t_1 = 0
+                last_residual = 2e2
+                residual = 1e2
+                if np.abs(new_z_1) > 1e-40:
+                    while np.abs(last_residual-residual) > self.rel_tol:
+                        last_residual = residual
+                        dt_2, dt_1 = __tar_grad_rot_lin__(pos_1, theta_1, new_x_1, new_z_1, pos_2, theta_2, new_x_2, t_2, t_1)
+                        t_2 -= self.k*dt_2
+                        t_1 -= self.k*dt_1
+                        residual = __tar_fun_rot_lin__(pos_1, theta_1, new_x_1, new_z_1, pos_2, theta_2, new_x_2, t_2, t_1)
+                else:
+                    while np.abs(last_residual-residual) > self.rel_tol:
+                        last_residual = residual
+                        dt_2, dt_1 = __tar_grad_lin_lin__(pos_1, theta_1, new_x_1, pos_2, theta_2, new_x_2, t_2, t_1)
+                        t_2 -= self.k*dt_2
+                        t_1 -= self.k*dt_1
+                        residual = __tar_fun_lin_lin__(pos_1, theta_1, new_x_1, pos_2, theta_2, new_x_2, t_2, t_1)
+                x, v = predict(pos_1, theta_1, new_x_1, new_z_1, t_1)
+                p = np.array([x[0, 0], x[2, 0]])
+                q = pos_2 + (p - pos_2) / np.linalg.norm(p - pos_2) * 1.1 * self.abs_tol  # 110% just to be sure
+                angle = get_rot_dir(theta_1, q, pos_2)
+                new_z_1 = (angle - theta_1) / t_1
+                collide3 = self.gradient_descent(pos_1, theta_1, new_x_1, new_z_1, pos_2, theta_2, new_x_2, new_z_2, time_exec)
+                if collide3:
+                    print 'collide3', pos_1, theta_1*180/np.pi, pos_2, theta_2*180/np.pi
+                    assd, dssa = predict(pos_1, theta_1, new_x_1, new_z_1, t_1)
+                    poss1 = np.array([assd[0, 0], assd[2, 0]])
+                    plt.plot(poss1[0], poss1[1], 'co')
 
-k = 0.3
-T = 3
-c_a = CollisionAvoidance(0.5**2, 1e-2**2, k)
-dsad = False
-while not dsad:
-    rob1 = [6*np.random.rand(2)-3, 2*np.pi*np.random.rand(), np.random.rand(), 2*np.random.rand()-1]
-    rob2 = [6*np.random.rand(2)-3, 2*np.pi*np.random.rand(), np.random.rand(), 2*np.random.rand()-1]
-    dsad = c_a.gradient_descent(rob1[0], rob1[1], rob1[2], rob1[3], rob2[0], rob2[1], rob2[2], rob2[3], T)
-time = np.linspace(0, T, 101)
-pos1 = np.zeros((2, 101))
-pos2 = np.zeros((2, 101))
-for i in range(0, len(time)):
-    asd, dsa = predict(rob1[0], rob1[1], rob1[2], rob1[3], time[i])
-    pos1[:, i] = np.array([asd[0, 0], asd[2, 0]])
-    asdd, dsad = predict(rob2[0], rob2[1], rob2[2], rob2[3], time[i])
-    pos2[:, i] = np.array([asdd[0, 0], asdd[2, 0]])
-plt.plot(pos1[0, :], pos1[1, :], 'g')
-plt.plot(pos2[0, :], pos2[1, :], 'b')
-plt.figure()
-
-a = np.linspace(-3, 3)
-b = np.linspace(-3, 3)
-A, B = np.meshgrid(a, b)
-# val = __tar_fun_lin_lin__(rob1[0], rob1[1], rob1[2], rob2[0], rob2[1], rob2[2], A, B)
-val = __tar_fun_rot_rot__(rob1[0], rob1[1], rob1[2], rob1[3], rob2[0], rob2[1], rob2[2], rob2[3], A, B)
-# val = __tar_fun_lin_rot__(rob1[0], rob1[1], rob1[2], rob2[0], rob2[1], rob2[2], rob2[3], A, B)
-# val = __tar_fun_rot_lin__(rob1[0], rob1[1], rob1[2], rob1[3], rob2[0], rob2[1], rob2[2], B, A)
-f = 0
-g = 0
-plt.plot(f, g, 'ro')
-for i in range(0, 300):
-    # v, u = __tar_grad_lin_lin__(rob1[0], rob1[1], rob1[2], rob2[0], rob2[1], rob2[2], f, g)
-    v, u = __tar_grad_rot_rot__(rob1[0], rob1[1], rob1[2], rob1[3], rob2[0], rob2[1], rob2[2], rob2[3], f, g)
-    # v, u = __tar_grad_lin_rot__(rob1[0], rob1[1], rob1[2], rob2[0], rob2[1], rob2[2], rob2[3], f, g)
-    # v, u = __tar_grad_rot_lin__(rob1[0], rob1[1], rob1[2], rob1[3], rob2[0], rob2[1], rob2[2], g, f)
-    f -= k*v
-    g -= k*u
-    if 0 > f or f > T or 0 > g or g > T:
-        print 'no collision'
-        break
-    plt.plot(f, g, 'ro')
-asd, dsa = predict(rob1[0], rob1[1], rob1[2], rob1[3], g)
-asdd, dsaa = predict(rob2[0], rob2[1], rob2[2], rob2[3], f)
-print asd[0, 0], asd[2, 0], '\n', asdd[0, 0], asdd[2, 0]
-CS = plt.contour(A, B, val, 100)
-plt.clabel(CS, inline=1, fontsize=10)
-plt.title('Simplest default with labels')
-plt.show()
+                    plt.plot(p[0], p[1], 'mo')
+                    plt.plot(q[0], q[1], 'ko')
+                    plt.plot(pos_1[0], pos_1[1], 'go')
+                    plt.plot(pos_2[0], pos_2[1], 'bo')
+                    time = np.linspace(0, time_exec, 101)
+                    pos1 = np.zeros((2, 101))
+                    pos2 = np.zeros((2, 101))
+                    for i in range(0, len(time)):
+                        asd, dsa = predict(pos_1, theta_1, new_x_1, new_z_1, time[i])
+                        pos1[:, i] = np.array([asd[0, 0], asd[2, 0]])
+                        asdd, dsad = predict(pos_1, theta_1, x_1, z_1, time[i])
+                        pos2[:, i] = np.array([asdd[0, 0], asdd[2, 0]])
+                    plt.plot(pos1[0, :], pos1[1, :], 'g')
+                    plt.plot(pos2[0, :], pos2[1, :], 'b')
+                    plt.axis('equal')
+                    plt.show()
+                    new_x_1 = 0
+        return new_x_1, new_z_1, new_x_2, new_z_2
